7d0ce2c8334c100b0277562182cb73bd88c13e2f
Zinhle <zinhle.nhlapo31@gmail.com>
2024-11-06 11:26:50 +0200

    Added css for the fashion and  lifestyle pages.

--- frontend/src/Pages/Fashion.jsx
+++ frontend/src/Pages/Fashion.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Fashion.css";
 
 const Fashion = () => {
   return (
@@ -7,19 +8,19 @@ const Fashion = () => {
       <div class="container">
         <div class="row">
           <div class="col-sm-6">
-            <img src="./fashion1.webp" alt="Chic style" />
+            <img src="./fashion1.webp" alt="Chic style" className="img-fluid"/>
             <h4>The Fig & Honey Edit</h4>
           </div>
           <div class="col-sm-6">
-            <img src="./fashion2.webp" alt="Country style" />
+            <img src="./fashion2.webp" alt="Country style" className="img-fluid shorter-image"/>
             <h4>DISSH - The Minimalistic Brand Worth The Hype</h4>
           </div>
           <div class="col-sm-6">
-            <img src="./fashion3.webp" alt="Pretty Yellow Dress" />
+            <img src="./fashion3.webp" alt="Pretty Yellow Dress" className="img-fluid"/>
             <h4>Spring Dressing With Poetry</h4>
           </div>
           <div class="col-sm-6">
-            <img src="./fashion4.webp" alt="Laid back style" />
+            <img src="./fashion4.webp" alt="Laid back style" className="img-fluid short-image"/>
             <h4>Pre-loved Styling Edit</h4>
           </div>
         </div>
--- frontend/src/Pages/Lifetsyle.jsx
+++ frontend/src/Pages/Lifetsyle.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Lifestyle.css"
 
 const Lifestyle = () => {
   return (
@@ -6,19 +7,19 @@ const Lifestyle = () => {
       <div class="container">
         <div class="row">
           <div class="col-sm-6">
-            <img src="./lifestyle1.webp" alt="A girl and her tulips." />
+            <img src="./lifestyle1.webp" alt="A girl and her tulips." className="img-fluid short-image"/>
             <h4>The Year Of BLOOM</h4>
           </div>
           <div class="col-sm-6">
-            <img src="./lifestyle2.webp" alt="Just sip some tea" />
+            <img src="./lifestyle2.webp" alt="Just sip some tea" className="img-fluid"/>
             <h4>The Words You Speak Become The House You Live In</h4>
           </div>
           <div class="col-sm-6">
-            <img src="./lifestyle3.webp" alt="You can't go wrong with a stripped shirt." />
+            <img src="./lifestyle3.webp" alt="You can't go wrong with a stripped shirt." className="img-fluid"/>
             <h4>The Journey Of Connection & Wellness</h4>
           </div>
           <div class="col-sm-6">
-            <img src="./lifestyle4.webp" alt="Monochrome magic" />
+            <img src="./lifestyle4.webp" alt="Monochrome magic" className="img-fluid"/>
             <h4>Haut Q&A</h4>
           </div>
         </div>
